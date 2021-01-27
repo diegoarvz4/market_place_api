5229a2d1faaef42e6ef7662a8e3ac69448c55107
diegoarvz4 <darvz.gtz@gmail.com>
2021-01-27 13:40:08 -0600

    Setup tokens controller

---
+++ app/controllers/api/v1/tokens_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::TokensController < ApplicationController
+  
+  def create
+    @user = User.find_by_email(user_params[:email])
+    # We use the method User#authenticate which exists
+    # thanks to the gem bcrypt
+    if @user&.authenticate(user_params[:password])
+      render json: {
+        token: JsonWebToken.encode(user_id: @user.id),
+        email: @user.email
+      }
+    else 
+      head :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
